6f7674dabcfd5103093cb9810f6c748238a66803
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-11 18:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function threeSum(arr, target) {
-// write your code here
+let num=n;
+	let value=0;
+	let base=1;
+	let temp=num;
+	while(temp){
+		let last=temp%10;
+		temp=Math.floor(temp/10)
+		value+=last*base;
+		base=base*2
+	}
+	return value
   
 }
 
