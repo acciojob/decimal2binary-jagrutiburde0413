442ca3b5f830e60d9ee5423244643c498850a643
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-24 17:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let decInput=document.getElementById("dec-input");
 let binInput=document.getElementById("bin-input");
-let errorMsg=document.getElementById("sample-error");
+//let errorMsg=document.getElementById("sample-error");
 
 decInput.addEventListener("input",()=>{
     let decValue=parseInt(decInput.value)
